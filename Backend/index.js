3379b5a56be3f5f37674267c82f9dd4834b2cee8
Kamal1234566 <kj9822477@gmail.com>
2025-07-12 14:35:24 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -3,19 +3,34 @@ import { connectdb } from "./mongodb.js";
 import routes from './Routes/router.js';
 import userRoute from "./userRoute.js";
 import cookieParser from 'cookie-parser';
-
 import cors from 'cors';
+
 const app = express();
 const port = 4000;
 
 (async () => {
   try {
-    const allowedOrigins = ['http://localhost:5173','https://mern-auth-project-rie8.onrender.com','https://mern-auth-nrxl.onrender.com'];
     await connectdb();
+
+    const allowedOrigins = [
+      'http://localhost:5173',
+      'https://mern-auth-project-rie8.onrender.com',
+      'https://mern-auth-nrxl.onrender.com',
+    ];
+
+    app.use(cors({
+      origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+      credentials: true,
+    }));
+
     app.use(express.json());
     app.use(cookieParser());
-
-    app.use(cors({ origin: allowedOrigins, credentials: true }));
 
     app.use('/api/auth', routes);
     app.use('/api/user', userRoute);
